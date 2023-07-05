655b59cc25ee969ba750e5b1f61ce3eef1f5f2f9
Emanuel <emanuelmissena12121@gmail.com>
2023-07-05 15:33:51 -0300

    Atualizado erro na compilação por requer o HikariCP sem uso.

--- src/main/java/com/emanuelvini/feastcore/common/storage/MySQL.java
+++ src/main/java/com/emanuelvini/feastcore/common/storage/MySQL.java
@@ -3,14 +3,6 @@ package com.emanuelvini.feastcore.common.storage;
 import com.emanuelvini.feastcore.common.storage.configuration.MySQLConfiguration;
 import com.henryfabio.sqlprovider.connector.SQLConnector;
 import com.henryfabio.sqlprovider.connector.type.impl.MySQLDatabaseType;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.val;
-import org.bukkit.configuration.Configuration;
-import org.bukkit.configuration.file.FileConfiguration;
 
 
 public class MySQL {
